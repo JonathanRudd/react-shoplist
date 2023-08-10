e95b5ece0fd95daa54f74d3c092ad6e781a45458
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-08-10 15:10:45 +0900

    bold text on entry function added

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,25 +3,33 @@ import { removeItem } from "../store";
 
 function ItemList() {
   const dispatch = useDispatch();
-  const items = useSelector(({ items: { data, searchTerm } }) => {
-    return data.filter((item) =>
+  const { items, name } = useSelector(({ form, items: { data, searchTerm } }) => {
+    const filteredItems = data.filter((item) =>
       item.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
+
+    return {
+      items: filteredItems,
+      name: form.name,
+    }
   });
 
-  const handleCarDelete = (item) => {
+  const handleItemDelete = (item) => {
     dispatch(removeItem(item.id));
   };
 
   const renderedItems = items.map((item) => {
+    // decide if item should be bold
+    const bold = name && item.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={item.id} className="panel">
+      <div key={item.id} className={`panel ${bold && 'bold'}`}>
         <p>
           {item.name} - {item.cost}円
         </p>
         <button
           className="button is-danger"
-          onClick={() => handleCarDelete(item)}
+          onClick={() => handleItemDelete(item)}
         >
           delete
         </button>
